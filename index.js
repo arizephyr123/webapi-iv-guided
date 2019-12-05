56f75c053cd381dee453a45add132f43627abc2a
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-05 09:33:43 -0800

    Initial Commit, changed port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(4009, () => {
+  console.log('\n*** Server Running on http://localhost:4009 ***\n');
 });
---
+++ notes.md
@@ -0,0 +1,7 @@
+# Deployment NOtes
+
+## Quotes
+>Developers get paid to  deliver value, not lines on code.
+>
+> &mdash; A pragmatic Developer
+
