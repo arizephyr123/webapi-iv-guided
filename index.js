b5426932aae87da80b45eb61b48701cb6d049ada
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-12-05 10:53:37 -0800

    .env added

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// needs to be earlies place in app as possible
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
+//reading an environment variable - not writing
+// use port provided by environment (heroku) || 4009
 const port = process.env.PORT || 4009;
 
 server.listen(port, () => {
